0d577550cd4dfd1ad5707c6117ffaf2ee767d18f
0xUgochukwu <0xUgochukwu@gmail.com>
2023-11-29 23:06:14 +0100

    await

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let response = {};
+
+  try {
+    response = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (e) {
+    response = { photo: null, user: null };
+  }
+
+  return response;
+}
