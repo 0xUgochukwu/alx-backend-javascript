f7ba6b85ca61dd650daf5fdc53023e7634f25109
0xUgochukwu <0xUgochukwu@gmail.com>
2023-11-29 22:47:51 +0100

    final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((res) => {
+    .then((res) => (
       res.map((r) => ({
         status: r.status,
         value: r.status === 'fulfilled' ? r.value : String(r.reason),
-      }));
-    });
+      }))
+    ));
 }
