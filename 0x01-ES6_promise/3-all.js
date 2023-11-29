e489256edd1dfa472e1508df196207cbab59df85
0xUgochukwu <0xUgochukwu@gmail.com>
2023-11-29 22:25:34 +0100

    signup2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return Promise
     .all([createUser(), uploadPhoto()])
     .then((res) => {
-      console.log(res[1].body, res[0].firstName, res[0].lastName);
+      console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
 }
