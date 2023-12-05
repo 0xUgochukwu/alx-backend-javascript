a007b5c2453f1edcc8b9b459897687fee5028a23
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-05 10:32:09 +0100

    update grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,16 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (students instanceof Array) {
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => (
+        {
+          ...student,
+          grade: (newGrades
+            .find((grade) => grade.studentId === student.id) || { grade: 'N/A' }
+          ).grade,
+        }
+      ));
+  }
+
+  return [];
+}
