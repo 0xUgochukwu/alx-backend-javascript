183cea7c75a062cf1ee624490b7f0768b671e650
0xUgochukwu <0xUgochukwu@gmail.com>
2023-12-05 13:16:40 +0100

    clean

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
+  return [...set]
+    .filter((el) => el.startsWith(startString))
+    .map((el) => el.slice(startString.length))
+    .join('-');
+}
