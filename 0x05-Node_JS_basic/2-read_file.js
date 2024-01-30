c1ee9dd5262e62502898ef296fccb7a9fcf0768e
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-30 08:19:43 +0100

    readfileSync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,36 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    const students = data.split('\n').filter((student) => student).slice(1);
-    const fields = {};
-    for (const student of students) {
-      const studentData = student.split(',');
-      const firstName = studentData[0];
-      const field = studentData[studentData.length - 1];
-      if (!fields[field]) {
-        fields[field] = {
-          count: 0,
-          students: []
-        };
-      }
-      fields[field].count += 1;
-      fields[field].students.push(firstName);
-    }
-    console.log(`Number of students: ${students.length}`);
-    for (const field of Object.keys(fields)) {
-      const { count, students } = fields[field];
-      console.log(
-        `Number of students in ${field}: ${count}. List: ${students.join(
-          ', '
-        )}`
-      );
-    }
-  } catch (e) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf8');
+  const students = data.split('\n').filter((student) => student).slice(1);
+  const fields = {};
+  for (const student of students) {
+    const studentData = student.split(',');
+    const firstName = studentData[0];
+    const field = studentData[studentData.length - 1];
+    if (!fields[field]) {
+      fields[field] = {
+        count: 0,
+        students: []
+      };
+    }
+    fields[field].count += 1;
+    fields[field].students.push(firstName);
+  }
+  console.log(`Number of students: ${students.length}`);
+  for (const field of Object.keys(fields)) {
+    const { count, students } = fields[field];
+    console.log(
+      `Number of students in ${field}: ${count}. List: ${students.join(
+        ', '
+      )}`
+    );
   }
 };
 
