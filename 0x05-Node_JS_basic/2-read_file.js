c25d6f3492daa8d296f821eec5bcc562172bcfbb
0xUgochukwu <0xUgochukwu@gmail.com>
2024-01-30 08:15:45 +0100

    readfileSync

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const countStudents = (path) => {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const students = data.split('\n').filter((student) => student).slice(1);
+    const fields = {};
+    for (const student of students) {
+      const studentData = student.split(',');
+      const firstName = studentData[0];
+      const field = studentData[studentData.length - 1];
+      if (!fields[field]) {
+        fields[field] = {
+          count: 0,
+          students: []
+        };
+      }
+      fields[field].count += 1;
+      fields[field].students.push(firstName);
+    }
+    console.log(`Number of students: ${students.length}`);
+    for (const field of Object.keys(fields)) {
+      const { count, students } = fields[field];
+      console.log(
+                `Number of students in ${field}: ${count}. List: ${students.join(
+                    ', '
+                )}`
+      );
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+};
+
+countStudents('database.csv');
+module.exports = countStudents;
